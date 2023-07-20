532bba8c5cd89f622fabbffa5c13f92d4b63141a
mszewcz3 <01140109@pw.edu.pl>
2023-07-20 20:27:38 +0200

    Better mark by pylint and rflint

--- firstTask/nokiaWiki.robot
+++ firstTask/nokiaWiki.robot
@@ -2,47 +2,40 @@
 Documentation    Test for nokia wiki finding
 Library    SeleniumLibrary
 
-
 *** Variables ***
 ${ENTER_KEY}    \ue007
-${wikipedia_name}  a[contains(text(),'wikipedia.org')
-${BROWSER}  Firefox
-${SEARCH_TERM}  nokia wikipedia
-
+${wikipedia_name}    a[contains(text(),'wikipedia.org')]
+${BROWSER}    Firefox
+${SEARCH_TERM}    nokia wikipedia
 
 *** Test Cases ***
 Test Nokia Wiki
-    [Documentation]    opening nokia wiki
-
+    [Documentation]    Test case to open Nokia Wikipedia
     Open Browser And Go To Google
     Handle Google Cookies
     Search For A Term In Google
     In Google Search Result Find Wikipedia
-    Wait For The Page To Fully Load
-
 
 *** Keywords ***
 Open Browser And Go To Google
+    [Documentation]    Open a browser and navigate to Google
     Create Webdriver    ${BROWSER}
     Set Selenium Speed    0.5
     Maximize Browser Window
     Go To    https://www.google.com/
 
 Handle Google Cookies
-    # Check if the cookie window is present
+    [Documentation]    Handle Google cookies window
     ${cookie_window_present}  Run Keyword And Return Status  Page Should Contain Element  id=W0wltc
-
-    # Handle cookies window. Cancel cookies
-    Run Keyword If  ${cookie_window_present}  Click Element  id=W0wltc
+    Run Keyword If    ${cookie_window_present}    Click Element    id=W0wltc
 
 Search For A Term In Google
-    Input Text  name=q  ${SEARCH_TERM}
-    Press Keys  name=q    ${ENTER_KEY}
+    [Documentation]    Search for a term in Google search box
+    Input Text    name=q    ${SEARCH_TERM}
+    Press Keys    name=q    ${ENTER_KEY}
 
 In Google Search Result Find Wikipedia
+    [Documentation]    Click on the first link that contains "wikipedia.org"
     Wait Until Page Contains    wikipedia.org
-    # Click on the first link that contains "wikipedia.org"
-    Click Link    xpath://a[contains(@href, 'wikipedia.org')][1]
-
-Wait For The Page To Fully Load
-    Wait For Condition	return document.readyState == "complete"
+    Click Link    xpath://${wikipedia_name}[1]
+    Wait For Condition    return document.readyState == "complete"
--- secondTask/test_calculator_add.py
+++ secondTask/test_calculator_add.py
@@ -1,43 +1,65 @@
+"""
+Module: test_calculator_add
+
+Contains test cases for the addition operation in the Calculator class.
+"""
+
 import pytest
 from calculator import Calculator
 
 
 class TestCalculator:
+    """
+    TestCalculator class for testing the Calculator class's addition operation.
+    """
+
     def setup_method(self):
+        """
+        Setup method to create a Calculator instance before each test method.
+        """
         self.calculator = Calculator()
 
     def test_add_positive_numbers(self):
+        """Test addition of positive numbers."""
         result = self.calculator.add(4, 3)
         assert result == 7
 
     def test_add_negative_numbers(self):
+        """Test addition of negative numbers."""
         result = self.calculator.add(-5, -6)
         assert result == -11
 
     def test_add_positive_and_negative_numbers(self):
+        """Test addition of positive and negative numbers."""
         result = self.calculator.add(4, -1)
         assert result == 3
 
     def test_add_zero_to_zero(self):
+        """Test addition of zero to zero."""
         result = self.calculator.add(0, 0)
         assert result == 0
 
     def test_add_zero_to_number(self):
+        """Test addition of zero to a number."""
         result = self.calculator.add(0, 9)
         assert result == 9
 
     def test_add_double_numbers(self):
+        """Test addition of floating-point numbers."""
         result = self.calculator.add(2.3, 2.47)
-        assert result == 4.77
+        assert result == pytest.approx(4.77, abs=1e-2)
 
     def test_add_big_numbers(self):
+        """Test addition of big numbers."""
         result = self.calculator.add(1000000000000002, 999999999999999)
-        assert 2000000000000001
+        assert result == 2000000000000001
 
     def test_add_big_negative_numbers(self):
+        """Test addition of big negative numbers."""
         result = self.calculator.add(-3000000000000002, 999999999999999)
-        assert -2000000000000003
+        assert result == -2000000000000003
 
     def test_add_invalid_input(self):
+        """Test addition with invalid input."""
         with pytest.raises(TypeError):
             self.calculator.add("3", 7)
